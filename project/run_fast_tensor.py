81b72529a84bae552b032ff544ac2f4b66e1d763
Olivia Mei <68489056+olivesmoo@users.noreply.github.com>
2024-11-18 23:37:22 -0500

    time index

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -103,7 +103,7 @@ class FastTrain:
                 out = self.model.forward(X).view(y.shape[0])
                 y2 = minitorch.tensor(data.y)
                 correct = int(((out.detach() > 0.5) == y2).sum()[0])
-                log_fn(epoch, total_loss, correct, losses, times)
+                log_fn(epoch, total_loss, correct, losses, times[epoch])
 
 
 if __name__ == "__main__":
